8f7f525828fc98749c607c819ebc80db9d48e5be
wanying04 <wanyingt@hotmail.com>
2025-10-20 17:19:26 +0200

    create function pwd, exit and new builtins file

--- src/executor/builtins.c
+++ src/executor/builtins.c
@@ -1,12 +1,6 @@
 #include "minishell.h"
 
-// void	handle_errores(void)
-// {
-// 	//list situations
-// 	return (1);
-// }
-
-int	buildin_echo(char **args)
+int	builtin_echo(char **args)
 {
 	int	i;
 	int	newline;
@@ -35,7 +29,7 @@ int	buildin_echo(char **args)
 	return (0);
 }
 
-int	buildin_cd(char **args)
+int	builtin_cd(char **args)
 {
 	if (!args[1])
 	{
@@ -55,3 +49,50 @@ int	buildin_cd(char **args)
 	return (0);
 }
 
+int	builtin_pwd(char **args)
+{
+	char	cwd[4096];
+
+	(void)args;
+
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	{
+		perror("pwd");
+		return (1);
+	}
+	printf("%s\n", cwd);
+	return (0);
+}
+
+int	builtin_exit(char **args)
+{
+	int	exit_status;
+
+	exit_status = 0;
+	if (args[1])
+		exit_status = ft_atoi(args[1]);
+	printf("exit\n");
+	exit(exit_status);
+}
+
+int	execute_builtins(char **args)
+{
+	if (!args || !args[0])
+		return (1);
+	if (ft_strncmp(args[0], "echo", 5) == 0 && args[0][4] == '\0')
+		return (builtin_echo(args));
+	else if (ft_strncmp(args[0], "cd", 3) == 0 && args[0][2] == '\0')
+		return (builtin_cd(args));
+	else if (ft_strncmp(args[0], "pwd", 4) == 0 && args[0][3] == '\0')
+		return (builtin_pwd(args));
+	else if (ft_strncmp(args[0], "export", 7) == 0 && args[0][6] == '\0')
+		return (builtin_export(args));
+	else if (ft_strncmp(args[0], "unset", 6) == 0 && args[0][5] == '\0')
+		return (builtin_unset(args));
+	else if (ft_strncmp(args[0], "env", 4) == 0 && args[0][3] == '\0')
+		return (builtin_env(args));
+	else if (ft_strncmp(args[0], "exit", 5) == 0 && args[0][4] == '\0')
+		return (builtin_exit(args));
+	else
+		return (1);
+}
