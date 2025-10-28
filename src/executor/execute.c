6e743b83fcabfb8e4c49efa94c3a9e9154a92656
wanying04 <wanyingt@hotmail.com>
2025-10-28 19:37:36 +0100

    mute all for testing

--- src/executor/execute.c
+++ src/executor/execute.c
@@ -1,24 +1,24 @@
-#include "minishell.h"
+// #include "minishell.h"
 
-int	execute(char **args, t_env *env)
-{
-	pid_t	pid;
-	int		status;
+// int	execute(char **args, t_env *env)
+// {
+// 	pid_t	pid;
+// 	int		status;
 
-	if (is_builtin(args[0]))
-		return (execute_builtins(args, env));
-	pid = fork();
-	if (pid == 0)
-	{
-		execve(path, args, env);
-		exit(127);
-	}
-	else if (pid > 0)
-	{
-		waitpid(pid, &status, 0);
-		return (WEXITSTATUS(status));
-	}
-	return (-1);
-}
+// 	if (is_builtin(args[0]))
+// 		return (execute_builtins(args, env));
+// 	pid = fork();
+// 	if (pid == 0)
+// 	{
+// 		execve(path, args, env);
+// 		exit(127);
+// 	}
+// 	else if (pid > 0)
+// 	{
+// 		waitpid(pid, &status, 0);
+// 		return (WEXITSTATUS(status));
+// 	}
+// 	return (-1);
+// }
 
-int	is_builtin()
+// int	is_builtin()
