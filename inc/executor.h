34d97ef0d14522a970161c3c6d1d259d39693b60
wanying04 <wanyingt@hotmail.com>
2025-10-28 19:35:34 +0100

    correct function name

--- inc/executor.h
+++ inc/executor.h
@@ -11,20 +11,22 @@ typedef struct s_env
 }	t_env;
 
 //builtins.c
-int		buildin_echo(char **args);
-int		buildin_cd(char **args);
-int		buildin_pwd(char **args);
-int		buildin_exit(char **args);
+int		builtin_echo(char **args);
+int		builtin_cd(char **args);
+int		builtin_pwd(char **args);
+int		is_integer(char *str);
+int		builtin_exit(char **args);
 int		execute_builtins(char **args, t_env *env);
 
 //builtins_env_variables.c
-t_env	*env_create(void);
-void	env_destroy(t_env *env);
 void	env_expand(t_env *env);
 void	env_set(t_env *env, const char *var_str);
+t_env	*env_create(void);
+void	env_destroy(t_env *env);
 void	env_unset(t_env *env, const char *name);
-int		buildin_env(char **args, t_env *env);
-int		buildin_export(char **args, t_env *env);
-int		buildin_unset(char **args, t_env *env);
+int		is_valid_var_name(char *str);
+int		builtin_env(char **args, t_env *env);
+int		builtin_export(char **args, t_env *env);
+int		builtin_unset(char **args, t_env *env);
 
 #endif
