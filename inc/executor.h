91d541b2e4bb86b8df2b34fe0be14371cb948dc3
wanying04 <wanyingt@hotmail.com>
2025-10-29 18:17:45 +0100

    create interfaces to connect main and executor

--- inc/executor.h
+++ inc/executor.h
@@ -3,28 +3,25 @@
 
 # include <sys/wait.h>
 
-typedef struct s_env
-{
-	char	**vars;
-	int		count;
-	int		capacity;
-}	t_env;
+// Forward declarations
+typedef struct s_command t_command;
+typedef struct s_env t_env;
+
+// ============ Executor 主接口函数 ============
+int		execute(t_command *cmd, t_env *env);
+int		execute_pipeline(t_command *cmd, t_env *env);
+int		handle_redirections(t_command *cmd);
+int		is_builtin_command(char **argv);
 
 //builtins.c
 int		builtin_echo(char **args);
-int		builtin_cd(char **args);
+int		builtin_cd(char **args, t_env *env);
 int		builtin_pwd(char **args);
 int		is_integer(char *str);
 int		builtin_exit(char **args);
 int		execute_builtins(char **args, t_env *env);
 
 //builtins_env_variables.c
-void	env_expand(t_env *env);
-void	env_set(t_env *env, const char *var_str);
-t_env	*env_create(void);
-void	env_destroy(t_env *env);
-void	env_unset(t_env *env, const char *name);
-int		is_valid_var_name(char *str);
 int		builtin_env(char **args, t_env *env);
 int		builtin_export(char **args, t_env *env);
 int		builtin_unset(char **args, t_env *env);
