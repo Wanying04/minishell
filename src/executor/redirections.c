bf04c38dc475930fb1a54041f07549253841a51a
wanying04 <wanyingt@hotmail.com>
2025-11-05 17:09:05 +0100

    add functions for external commands

--- src/executor/redirections.c
+++ src/executor/redirections.c
@@ -1,40 +1,76 @@
 #include "minishell.h"
 
+/*
+ * handle_redirections - 处理重定向
+ * 
+ * 功能：根据 cmd->redirects 设置文件描述符
+ * 
+ * 输入：t_command *cmd - 包含重定向信息的命令
+ * 返回：0 成功，-1 失败
+ */
+// int	handle_redirections(t_command *cmd)
+// {
+// 	// TODO: Executor 团队实现
+// 	(void)cmd;
+// 	return (0);
+// }
+
 int	handle_redirections()
 {
 
 }
 
+char **env_to_array(t_env *env);
+//用来把链表（或哈希表）环境变量转成 char **
+
+reset_signals_to_default()
+
+find_command_path()
+
+
+
+void	child_process(t_command *cmd, t_env *env)
+{
+	char	*path;
+
+	reset_signals_to_default();
+	handle_redirections(cmd);
+	path = find_command_path(cmd->argv[0], env);
+	if (!path)
+	{
+		write(2, cmd->argv[0], ft_strlen(cmd->argv[0]));
+		write(2, ": command not found\n", 20);
+		exit(127);
+	}
+	if (execve(path, cmd->argv, env_to_array(env)) == -1)
+	{
+		perror(cmd->argv[0]);
+		free(path);
+		exit(127);
+	}
+}
+
 int	execute_external_command(t_command *cmd, t_env *env)
 {
-	char	*input;
-
 	pid_t	pid;
+	int		status;
 
+	status = 0;
 	pid = fork();
-
 	if (pid < 0)
 	{
 		perror("fork");
 		return (1);
 	}
 	else if (pid == 0)
-	{
-		command_ls();
-		printf("Hi! I am child process %d\n", pid);
-	}
-		
+		child_process(cmd, env);
 	else
-		printf("Hi! Parent is here, %d\n", pid);
-	printf("Hi! Process ID is %d\n", pid);
-	return (0);
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		else if (WIFSIGNALED(status))
+			return (128 + WTERMSIG(status));
+	}
+	return (status);
 }
-
-// execute_external_command()
-// 负责「真正执行命令」：
-// fork();
-// execve();
-// 错误处理、退出状态；
-// 父进程等待子进程。
-
-
