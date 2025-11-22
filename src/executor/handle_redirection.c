ddaa69150669ab6ed8690c8af445bf041462fec9
wtang <wtang@student.42malaga.com>
2025-11-22 20:24:06 +0100

    modify analyze_redirections

--- src/executor/handle_redirection.c
+++ src/executor/handle_redirection.c
@@ -35,8 +35,14 @@ int	handle_redirections(t_command *cmd)
 	t_redir_config	redir_config;
 
 	ft_memset(&redir_config, 0, sizeof(redir_config));
-	if (analyze_redirections(cmd, &redir_config) != SUCCESS)
+	redir_config.delimiters = malloc(sizeof(char *) * cmd->redirect_count);
+	if (!redir_config.delimiters)
 		return (FAILURE);
+	if (analyze_redirections(cmd, &redir_config) != SUCCESS)
+	{
+		free(redir_config.delimiters);
+		return (FAILURE);
+	}
 	if (execute_redirections(cmd, &redir_config) != SUCCESS)
 	{
 		cleanup_redirections(&redir_config);
@@ -49,57 +55,35 @@ int	handle_redirections(t_command *cmd)
 int	analyze_redirections(t_command *cmd, t_redir_config *config)
 {
 	int	i;
-	int	final_input_exists;
-	int	final_output_exists;
+	int	delim_index;
 
-	if (!cmd || !cmd->argv || !cmd->argv[0])
+	if (!cmd || !config)
 		return (FAILURE);
-	final_input_exists = 0;
-	final_output_exists = 0;
-	if (cmd->redirects[cmd->redirect_count - 1].type == REDIR_IN || cmd->redirects[cmd->redirect_count - 1].type == REDIR_HEREDOC)
+	i = cmd->redirect_count - 1;
+	delim_index = 0;
+	while (i >= 0)
 	{
-		config->final_input = cmd->redirects[cmd->redirect_count - 1].file;
-		i = 1;
-		while (i < cmd->redirect_count)
+		if (!config->final_input && (cmd->redirects[i].type == REDIR_IN || cmd->redirects[i].type == REDIR_HEREDOC))
+			config->final_input = cmd->redirects[i].file;
+		if (!config->final_output && (cmd->redirects[i].type == REDIR_OUT || cmd->redirects[i].type == REDIR_APPEND))
 		{
-			if (cmd->redirects[cmd->redirect_count - 1 - i].type == REDIR_OUT || cmd->redirects[cmd->redirect_count - 1 - i].type == REDIR_APPEND)
-			{
-				config->final_output = cmd->redirects[cmd->redirect_count - 1 - i].file;
-				final_output_exists = 1;
-				break ;
-			}
-			i++;
+			config->final_output = cmd->redirects[i].file;
+			config->output_mode = (cmd->redirects[i].type == REDIR_APPEND);
 		}
-		if (!final_output_exists)
-			config->final_output = NULL;
-	}
-	else
-	{
-		config->final_output = cmd->redirects[cmd->redirect_count - 1].file;
-		i = 1;
-		while (i < cmd->redirect_count)
-		{
-			if (cmd->redirects[cmd->redirect_count - 1 - i].type == REDIR_IN || cmd->redirects[cmd->redirect_count - 1].type == REDIR_HEREDOC)
-			{
-				config->final_input = cmd->redirects[cmd->redirect_count - 1 - i].file;
-				final_input_exists = 1;
-				break ;
-			}
-			i++;
-		}
-		if (!final_input_exists)
-			config->final_input = NULL;
+		i--;
 	}
 	i = 0;
 	while (i < cmd->redirect_count)
 	{
 		if (cmd->redirects[i].type == REDIR_HEREDOC)
 		{
-			config->delimiters[i] = cmd->redirects[i].file;
+			config->delimiters[delim_index] = cmd->redirects[i].file;
 			config->delim_count++;
+			delim_index++;
 		}
 		i++;
 	}
+	return (SUCCESS);
 }
 
 int	execute_redirections(t_command *cmd, t_redir_config *config)
