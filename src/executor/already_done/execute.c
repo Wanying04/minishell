abda83ef83960a9b6d58ded885f11ccf65d63c71
wtang <wtang@student.42malaga.com>
2025-12-03 20:32:53 +0100

    changes in format

--- src/executor/already_done/execute.c
+++ src/executor/already_done/execute.c
@@ -3,11 +3,11 @@
 int	execute(t_command *cmd, t_env *env)
 {
 	if (!cmd || !cmd->argv)
-		return (0);
+		return (SUCCESS);
 	if (!cmd->argv[0] && cmd->redirect_count > 0)
 		return (handle_redirections_only(cmd));
 	if (!cmd->argv[0])
-		return (0);
+		return (SUCCESS);
 	if (cmd->is_piped == 1)
 		return (execute_pipeline(cmd, env));
 	else
