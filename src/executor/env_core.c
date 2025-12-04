0a1afd621cde4231d75e1b080245d71c21dfdf61
wtang <wtang@student.42malaga.com>
2025-12-04 11:20:47 +0100

    main functions of env

---
+++ src/executor/env_core.c
@@ -0,0 +1,67 @@
+#include "minishell.h"
+
+t_env	*env_create(char **envp)
+{
+	t_env	*env;
+	int		i;
+
+	env = malloc(sizeof(t_env));
+	env->capacity = 100;
+	env->count = 0;
+	env->exit_status = SUCCESS;
+	env->vars = malloc(sizeof(char *) * env->capacity);
+	env->vars[0] = NULL;
+	env->pwd = malloc(sizeof(t_pwd));
+	env->pwd->pwd = NULL;
+	env->pwd->oldpwd = NULL;
+	if (!envp)
+		return (env);
+	i = 0;
+	while (envp[i])
+	{
+		env_set(env, envp[i]);
+		i++;
+	}
+	return (env);
+}
+
+void	env_destroy(t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (i < env->count)
+	{
+		free(env->vars[i]);
+		i++;
+	}
+	free(env->vars);
+	if (env->pwd)
+	{
+		if (env->pwd->pwd)
+			free(env->pwd->pwd);
+		if (env->pwd->oldpwd)
+			free(env->pwd->oldpwd);
+		free(env->pwd);
+	}
+	free(env);
+}
+
+void	env_expand(t_env *env)
+{
+	int		new_capacity;
+	char	**new_vars;
+	int		i;
+
+	new_capacity = env->capacity * 2;
+	new_vars = malloc(sizeof(char *) * new_capacity);
+	i = 0;
+	while (i < env->count)
+	{
+		new_vars[i] = env->vars[i];
+		i++;
+	}
+	free(env->vars);
+	env->vars = new_vars;
+	env->capacity = new_capacity;
+}
