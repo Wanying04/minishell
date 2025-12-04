0cac87cf4dd340b5b667edec99192e52d4a29b07
wtang <wtang@student.42malaga.com>
2025-12-04 13:05:11 +0100

    manage long lines

--- src/executor/already_done/execute_pipeline.c
+++ src/executor/already_done/execute_pipeline.c
@@ -6,14 +6,16 @@ static int	is_valid_pipeline(t_command *cmd)
 	{
 		if (!cmd || !cmd->argv || !cmd->argv[0])
 		{
-			ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", STDERR_FILENO);
+			ft_putstr_fd("minishell: syntax error near unexpected token `|'\\n",
+				STDERR_FILENO);
 			return (FAILURE);
 		}
 		cmd = cmd->next;
 	}
 	if (!cmd || !cmd->argv || !cmd->argv[0])
 	{
-		ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: syntax error near unexpected token `|'\\n",
+			STDERR_FILENO);
 		return (FAILURE);
 	}
 	return (SUCCESS);
